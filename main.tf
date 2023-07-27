dde826b844bcf2c863c79ef95f5cc802fe3c64f8
Dário Matos <dario.matos@ua.pt>
2023-07-27 17:39:16 +0100

    Terraform config

--- main.tf
+++ main.tf
@@ -10,14 +10,73 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-830c94e3"
-#   instance_type = "t2.micro"
+resource "aws_vpc" "rio-vpc" {
+  cidr_block = "10.0.0.0/23"
+  tags = {
+    "Name" = "rio-vpc"
+    }
+}
 
-#   tags = {
-#     Name = "ExampleAppServerInstance"
-#   }
-# }
+resource "aws_internet_gateway" "rio-gateway" {
+  tags     = {
+    "Name" = "rio-gateway"
+    } 
+  vpc_id   = "vpc-04643ce6f5e2a1446"
+}
+
+resource "aws_nat_gateway" "rio-nat1" {
+  connectivity_type    = "public"
+  subnet_id = aws_subnet.rio-public1.id
+  private_ip           = "10.0.0.117"
+  tags = {
+    "Name" = "rio-nat1"
+    }
+}
+
+resource "aws_nat_gateway" "rio-nat2" {
+  connectivity_type    = "public"
+  subnet_id = aws_subnet.rio-public2.id
+  private_ip           = "10.0.0.165"
+  tags = {
+    "Name" = "rio-nat2"
+    }
+}
+
+resource "aws_subnet" "rio-public1" {
+  availability_zone                              = "us-east-1a"
+  cidr_block                                     = "10.0.0.0/25"
+  tags                                           = {
+    "Name" = "rio-public1"
+    }
+  vpc_id                                         = "vpc-04643ce6f5e2a1446"
+}
+
+resource "aws_subnet" "rio-public2" {
+  availability_zone                              = "us-east-1b"
+  cidr_block                                     = "10.0.0.128/25"
+  tags                                           = {
+    "Name" = "rio-public2"
+    }
+  vpc_id                                         = "vpc-04643ce6f5e2a1446"
+}
+
+resource "aws_subnet" "rio-private1" {
+  availability_zone                              = "us-east-1a"
+  cidr_block                                     = "10.0.1.0/25"
+  tags                                           = {
+    "Name" = "rio-private1"
+    }
+  vpc_id                                         = "vpc-04643ce6f5e2a1446"
+}
+
+resource "aws_subnet" "rio-private2" {
+  availability_zone                              = "us-east-1b"
+  cidr_block                                     = "10.0.1.128/25"
+  tags                                           = {
+    "Name" = "rio-private2"
+    }
+  vpc_id                                         = "vpc-04643ce6f5e2a1446"
+}
